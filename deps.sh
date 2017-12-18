e21361649c530c9414e9be5e26ed7e0f685ce591
Ricky Cook <mail@thatpanda.com>
2017-12-18 16:08:28 +1100

    Make wrapper bash

--- deps.sh
+++ deps.sh
@@ -4,11 +4,11 @@ set -e
 
 if which apt-get 2>&1 >/dev/null ; then
   apt-get update
-  apt-get install -y jq curl
+  apt-get install -y jq curl bash
 elif which yum 2>&1 >/dev/null ; then
-  yum install jq curl
+  yum install jq curl bash
 elif which apk 2>&1 >/dev/null ; then
-  apk add --no-cache jq curl
+  apk add --no-cache jq curl bash
 else
   echo Unsupported platform >&2
   exit 1
